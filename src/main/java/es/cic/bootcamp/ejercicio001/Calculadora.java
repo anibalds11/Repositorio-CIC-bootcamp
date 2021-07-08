5d3f7cf72519ac66489d9adb35077bea838cbc29
anibal <anibalds11@gmail.com>
2021-07-08 14:04:58 +0200

    Versión prueba GIT de test untario

---
+++ src/main/java/es/cic/bootcamp/ejercicio001/Calculadora.java
@@ -0,0 +1,10 @@
+package es.cic.bootcamp.ejercicio001;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/es/cic/bootcamp/ejercicio001/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package es.cic.bootcamp.ejercicio001;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
